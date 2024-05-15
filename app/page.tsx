cf484f644696cd8bb36aab536be7e857ae56b514
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-05-15 23:49:35 +0000

    Setup LandingPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Features from "@/components/landingpage/Features";
+import Header from "@/components/landingpage/Header";
+import Herotop from "@/components/landingpage/Herotop";
+import Plateform from "@/components/landingpage/Plateform";
+import Product from "@/components/landingpage/Product";
+import { Bento } from "@/components/landingpage/Bento";
+import CTA from "@/components/landingpage/CTA";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Herotop />
+      <Plateform />
+      <Features />
+      <Product />
+      <Bento />
+      <CTA />
+    </main>
+  );
 }
---
+++ components/landingpage/Plateform.tsx
@@ -0,0 +1,32 @@
+import Container from "@/components/elements/Container";
+import Image from "next/image";
+import Illustration_Dashbard from "@/public/images/illustration-dashboard.png";
+import Predict_Analytic from "@/public/images/predict-analytic.svg";
+
+export default function Plateform() {
+  return (
+    <Container>
+      <div className="mb-10 mt-10 relative py-4">
+        <div className="absolute -top-[4rem] right-3 z-30">
+          <Image src={Predict_Analytic} alt="prediction analytique" />
+        </div>
+        <div className="flex justify-center">
+          <Image
+            src={Illustration_Dashbard}
+            alt="Illustration Dashboard"
+            width={1000}
+            className="border border-gray-500 border-8 rounded-xl"
+          />
+        </div>
+        <div className="absolute z-20 top-[20rem] left-[32rem]">
+          <button
+            className="px-8 py-2 border-none bg-color_primary hover:bg-color_primary/90
+               text-white rounded-full font-bold"
+          >
+            Voir la démo
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+}

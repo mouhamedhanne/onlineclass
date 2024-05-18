5e3f6eb8d9f4726ac2cec2e03cf670ccadb96949
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-05-18 13:37:54 +0000

    update UI

--- components/landingpage/Footer.tsx
+++ components/landingpage/Footer.tsx
@@ -7,8 +7,70 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <Container>
-      <footer className=" mt-20 flex justify-between">
-        <div className=" mb-3">
+      <footer className="">
+        <div className="max-w-4xl flex-wrap w-full flex gap-4 m-auto px-4 pb-10">
+          <div className="w-full flex justify-between max-lg:flex-col">
+            <div className="flex flex-col gap-4">
+              <div className="space-y-1">
+                <Image
+                  src={LogoOnlineClass}
+                  alt="Logo OnlineClass"
+                  width={75}
+                />
+                <h3 className="scroll-m-20 text-xl text-color_title font-semibold tracking-tight">
+                  OnlineClass Pro
+                </h3>
+              </div>
+              <p className="text-sm text-muted-foreground italic">
+                © 2024 OnlineClass - All rights reserved.
+              </p>
+            </div>
+            <div className="flex flex-col items-start mt-4 lg:mt-0 lg:items-end gap-4">
+              <p className="text-lg text-white font-semibold">Legal</p>
+              <Link
+                href="/"
+                className="text-sm text-muted-foreground hover:underline"
+              >
+                Terms
+              </Link>
+              <Link
+                href="/"
+                className="text-sm text-muted-foreground hover:underline"
+              >
+                Privacy
+              </Link>
+            </div>
+
+            <div className="flex flex-col items-start mt-4 lg:mt-0 lg:items-end gap-4">
+              <p className="text-lg text-white font-semibold">Social</p>
+              <Link
+                href="/"
+                className="text-sm text-muted-foreground hover:underline"
+              >
+                LinkedIn
+              </Link>
+              <Link
+                href="/"
+                className="text-sm text-muted-foreground hover:underline"
+              >
+                Twitter
+              </Link>
+              <Link
+                href="/"
+                className="text-sm text-muted-foreground hover:underline"
+              >
+                Instagram
+              </Link>
+              <Link
+                href="/"
+                className="text-sm text-muted-foreground hover:underline"
+              >
+                Facebook
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
           <p className="text-base text-neutral-600 flex items-center justify-center gap-1">
             Propulsé avec <Heart className="text-red-500 " />
             par
@@ -16,16 +78,6 @@ export default function Footer() {
               <u>@OnlineClass.pro</u>
             </Link>
           </p>
-        </div>
-        <div>@Copywright 2024</div>
-        <div className="flex items-center space-x-2">
-          <Link href="/" className="hover:underline">
-            Privacy
-          </Link>
-          <Link href="/" className="hover:underline">
-            Terms
-          </Link>
-          <Image src={LogoOnlineClass} alt="Logo OnlineClass" width={20} />
         </div>
       </footer>
     </Container>
